47111d6ec279af938042496fcd319a2830553c31
Yanxing Wang <yanxingwang@Yanxings-MBP.localdomain>
2026-09-18 18:33:23 -0700

    feat(nvim): use absolute line numbers (disable relativenumber)

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,6 @@
+-- Custom options, loaded on top of LazyVim's defaults.
+-- (LazyVim auto-loads lua/config/options.lua)
+
+-- Use absolute line numbers instead of LazyVim's default relative numbers.
+vim.opt.relativenumber = false
+vim.opt.number = true
